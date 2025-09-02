b5457982b11d08ceb2e22c7708e510af91df4aa9
EC2 Default User <bjking102409@gmail.com>
2025-09-02 12:42:43 +0000

    firstSubmit

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 
+let num = prompt("입력:");
+if (num > 0) {
+  console.log("양수");
+} else if (num < 0) {
+  console.log("음수");
+} else {
+  console.log("0");
+}
+
+
+let score = prompt("Your score:");
+let grade;
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+} else if (score >= 70) {
+  grade = "C";
+} else if (score >= 60) {
+  grade = "D";
+} else {
+  grade = "F";
+}
+console.log("Your grade: " + grade);
+
+
+
+let skills = ["HTML", "CSS", "Javascript", "React"];
+if (skills.includes("Javascript") && skills.includes("React")) {
+  console.log("합격");
+} else if (skills.includes("Javascript") || skills.includes("React")) {
+  console.log("예비");
+} else {
+  console.log("탈락");
+}
