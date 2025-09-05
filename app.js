e84a14757574f950303c823784c1aeb3ce65570a
EC2 Default User <bjking102409@gmail.com>
2025-09-05 02:24:22 +0000

    firstSubmit

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
+// 1부터 100까지 더하는 for문을 만들고 결과를 출력하시오.
+let numA = 0
 
+for(let i = 1; i<= 100; i++){
+  numA +=i;
+}
+
+console.log("1부터 100까지의 합:",numA);
+
+// 1부터 100까지 홀수만 출력하자.
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+
+// 1부터 50까지 369결과를 프린트하자.
+for (let i = 1; i <= 50; i++) {
+  let str = String(i);
+  let clapCount = 0;
+
+  for (let ch of str) {
+    if (ch === "3" || ch === "6" || ch === "9") {
+      clapCount++;
+    }
+  }
+
+  if (clapCount > 0) {
+    console.log("짝".repeat(clapCount)); 
+  } else {
+    console.log(i);
+  }
+}
+
+
+//주어진 숫자가 소수이면 true 아니면 false를 출력하는 프로그램을 짜시오.
+
+let num = 29; 
+let isPrime = true;
+
+if (num <= 1) {
+  isPrime = false;
+} else {
+  for (let i = 2; i < num; i++) { 
+    if (num % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+}
+
+if (isPrime) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+  
